b3fa8febb6bda3447cb042285506cf4f1fa5cb43
Nikolay Semyachkin <luoji@yandex-team.ru>
2022-06-07 02:15:19 +0300

    some wip

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ from sklearn.linear_model import LogisticRegression
 import string
 
 
-DEFAULT_COONFIG = {
+DEFAULT_CONFIG = {
     "endpoint": "score",
     "text_field": "text",
     "model_path": "models/log_reg.joblib",
     "vectorizer_path": "models/tfidf_vect.joblib"
 }
+
 
 def preprocess(text):
     return ''.join(c if c not in string.punctuation else f' {c} ' for c in text.lower() )
@@ -20,17 +21,17 @@ def preprocess(text):
 app = Flask(__name__)
 
 
-@app.get(f"/{DEFAULT_COONFIG['endpoint']}")
-def get_score():
-    text = request.args.get("text")
-    text_vec = vect.transform([preprocess(text)])
-    return jsonify({
-        "text": text,
-        "is_toxic": float(clf.predict_proba(text_vec)[:,1])
-    })
-
-
 if __name__ == '__main__':
+
+    @app.get(f"/{DEFAULT_CONFIG['endpoint']}")
+    def get_score():
+        text = request.args.get("text")
+        text_vec = vect.transform([preprocess(text)])
+        return jsonify({
+            "text": text,
+            "is_toxic": float(clf.predict_proba(text_vec)[:,1])
+        })
+
     clf = joblib.load(DEFAULT_CONFIG['model_path'])
     vect = joblib.load(DEFAULT_CONFIG['vectorizer_path'])
     app.run(debug=True, host='0.0.0.0')
